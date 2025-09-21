9747bca56e3a02bd62fb14c1ce679a5d85712041
DynaTiuM <raphael.perrin754@gmail.com>
2025-09-21 23:29:14 +0200

    fix: trying to make the app working on embedded discord activity

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Layout from "./components/Layout/Layout";
-
 export default function App() {
   //const { user, loading } = useDiscordUser();
 
   //if (loading) return <p>Loading Discord user...</p>;
   //if (!user) return <p>Failed to load Discord user</p>;
   return (
-  <>
+  <div className="min-h-screen">
     <h2>Welcome</h2>
-    <Layout />
-  </>);
+  </div>);
 }
