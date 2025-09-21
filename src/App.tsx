9b3c62838d0c015117d7a6d69a4e4291574472dd
DynaTiuM <raphael.perrin754@gmail.com>
2025-09-22 01:01:59 +0200

    feat: Discord OAuth2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Layout from "./components/Layout/Layout";
+import { useDiscordUser } from "./hooks/useDiscordUser";
 
 export default function App() {
-  //const { user, loading } = useDiscordUser();
+  const { user, loading } = useDiscordUser();
 
-  //if (loading) return <p>Loading Discord user...</p>;
-  //if (!user) return <p>Failed to load Discord user</p>;
+  if (loading) return <p>Loading Discord user...</p>;
+  if (!user) return <p>Failed to load Discord user</p>;
   return (
   <div className="min-h-screen">
     <h2>Welcome</h2>
